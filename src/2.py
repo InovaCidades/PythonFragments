d03c372cc6dab0c7bbcab272161c3ce6bd300917
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-03-14 20:34:28 -0300

    Create 2.py

---
+++ src/2.py
@@ -0,0 +1,9 @@
+#Set the following variables to the corresponding values:
+#my_int to the value 7
+#my_float to the value 1.23
+#my_bool to the value True
+
+
+my_int = 7
+my_float = 1.23
+my_bool = True
