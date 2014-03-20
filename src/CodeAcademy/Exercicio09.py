c7b1765ad61ef87ac44b4ed9b31770fd38bb07a3
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-03-20 12:25:20 -0300

    Create Exercicio09.py

---
+++ src/CodeAcademy/Exercicio09.py
@@ -0,0 +1,14 @@
+#Let's put our knowledge to work.
+
+#Write a single-line comment on line 1. It can be anything! (Make sure it starts with #)
+#Set the variable monty equal to True.
+#Set another variable python equal to 1.234.
+#Set a third variable monty_python equal to python squared.
+
+
+#blabla
+
+monty = True
+python = 1.234
+
+monty_python = python ** 2
