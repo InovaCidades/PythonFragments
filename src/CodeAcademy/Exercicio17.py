5b8cef064610336500961e64c821ecfdb7480294
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-04-02 01:26:25 -0300

    Create Exercicio17.py

---
+++ src/CodeAcademy/Exercicio17.py
@@ -0,0 +1,16 @@
+#On line 13, assign the variable fifth_letter equal to the fifth letter of the string "MONTY".
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
