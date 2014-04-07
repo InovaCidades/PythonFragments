d6b9b0485f883bd79b360a8d62bc25832c1cace5
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-04-07 11:54:07 -0300

    Create Exercicio21.py

---
+++ src/CodeAcademy/03 - Strings & Console Output/Exercicio21.py
@@ -0,0 +1,10 @@
+#Create a variable pi and set it to 3.14 on line 4.
+#Call str(pi) on line 5, after print.
+
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+
+pi = 3.14
+
+print str(pi)
+print 
