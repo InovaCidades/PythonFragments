f383c5ca6be5c538e9155e859c6ea304497125bd
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-04-15 13:03:31 -0300

    Create Exercicio29.py

---
+++ src/CodeAcademy/03 - Strings & Console Output/Exercicio29.py
@@ -0,0 +1,6 @@
+#Take a look at the code in the editor. What do you think it'll do? Click Save & Submit when you think you know.
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
