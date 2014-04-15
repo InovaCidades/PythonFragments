4397b4170a55054e37623c446348692085571d61
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-04-15 13:05:34 -0300

    Create Exercicio31.py

---
+++ src/CodeAcademy/03 - Strings & Console Output/Exercicio31.py
@@ -0,0 +1,11 @@
+#Let's wrap it all up!
+
+#On line 3, create the variable my_string and set it to any string you'd like.
+#On line 4, print the length of my_string.
+#On line 5, print the .upper() case version of my_string.
+
+# Write your code below, starting on line 3!
+
+my_string = "blabla"
+print len(my_string)
+print my_string.upper()
