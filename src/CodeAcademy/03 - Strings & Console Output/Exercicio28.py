756daa5e4421a631a147567d6ece75725c387087
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-04-15 13:00:32 -0300

    Create Exercicio28.py

---
+++ src/CodeAcademy/03 - Strings & Console Output/Exercicio28.py
@@ -0,0 +1,6 @@
+#Run the code as-is. You get an error!
+#Use str() to turn 3.14 into a string. Then run the code again.
+
+# Turn 3.14 into a string on line 3!
+
+print "The value of pi is around " + str(3.14)
