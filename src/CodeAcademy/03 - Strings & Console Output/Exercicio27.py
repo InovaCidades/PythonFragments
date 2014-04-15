3138dbe6e4bb7d3f238baa5770cd8e1c356e0b3a
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-04-15 12:59:44 -0300

    Create Exercicio27.py

---
+++ src/CodeAcademy/03 - Strings & Console Output/Exercicio27.py
@@ -0,0 +1,6 @@
+#Let's give it a try. Print the concatenated strings "Spam ", "and ", "eggs" on line 3, just like the example above.
+#Make sure you include the spaces after "Spam " and "and ".
+
+# Print the concatenation of "Spam and eggs" on line 3!
+
+print "Spam " + "and " + "eggs"
