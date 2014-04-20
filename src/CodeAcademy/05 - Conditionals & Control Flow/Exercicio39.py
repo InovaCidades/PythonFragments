2a9c51042cb9387696e810699f734f1cbb346545
Gabriel Garcia <GabrielGarcia1@users.noreply.github.com>
2014-04-19 22:12:58 -0300

    Create Exercicio39.py

---
+++ src/CodeAcademy/05 - Conditionals & Control Flow/Exercicio39.py
@@ -0,0 +1,24 @@
+#For each boolean value in the editor, write an expression that evaluates to that value.
+
+#Remember, comparators are: ==, !=, >, >=, <, and <=.
+
+#Use at least three different ones!
+
+#Don't just use True and False! That's cheating!
+
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 5 < 6
+
+# Make me false!
+bool_two = 5 <= 4
+
+# Make me true!
+bool_three = 5 != 4 
+
+# Make me false!
+bool_four = 6 <= 5
+
+# Make me true!
+bool_five = 6 > 5
