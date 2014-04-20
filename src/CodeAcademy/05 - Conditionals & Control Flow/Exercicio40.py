c9f2410b3e7a139a0cd4be1150e3b8a7ace9517a
Gabriel Garcia <GabrielGarcia1@users.noreply.github.com>
2014-04-20 19:26:35 -0300

    Create Exercicio40.py

---
+++ src/CodeAcademy/05 - Conditionals & Control Flow/Exercicio40.py
@@ -0,0 +1,17 @@
+#Let's practice with and. Assign each variable to the appropriate boolean value.
+
+#Set bool_one equal to the result of False and False
+#Set bool_two equal to the result of -(-(-(-2))) == -2 and 4 >= 16**0.5
+#Set bool_three equal to the result of 19 % 4 != 300 / 10 / 10 and False
+#Set bool_four equal to the result of -(1**2) < 2**0 and 10 % 10 <= 20 - 10 * 2
+#Set bool_five equal to the result of True and True
+
+bool_one = False
+
+bool_two = False
+
+bool_three = False
+
+bool_four = True
+
+bool_five = True
