6130f14915045befe80519ae7bb5c10e6fe5152b
Gabriel Garcia <GabrielGarcia1@users.noreply.github.com>
2014-04-21 21:59:22 -0300

    Create Exercicio41.py

---
+++ src/CodeAcademy/05 - Conditionals & Control Flow/Exercicio41.py
@@ -0,0 +1,17 @@
+#Let's get some practice with not.
+
+#Set bool_one equal to the result of not True
+#Set bool_two equal to the result of not 3**4 < 4**3
+#Set bool_three equal to the result of not 10 % 3 <= 10 % 2
+#Set bool_four equal to the result of not 3**2 + 4**2 != 5**2
+#Set bool_five equal to the result of not not False
+
+bool_one = False
+
+bool_two = True
+
+bool_three = True
+
+bool_four = True
+
+bool_five = False
