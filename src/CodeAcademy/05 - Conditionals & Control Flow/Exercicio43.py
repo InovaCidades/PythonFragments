bcbb2f13ad09868d7f0f47be929d17f3ffdcde69
Gabriel Garcia <gabrielgfa@users.noreply.github.com>
2014-04-30 13:19:04 -0300

    Create Exercicio43.py

---
+++ src/CodeAcademy/05 - Conditionals & Control Flow/Exercicio43.py
@@ -0,0 +1,20 @@
+#This time we'll give the expected result, and you'll use some combination of boolean operators to achieve that result.
+
+#Remember, the boolean operators are and, or, and not. Use each one at least once!
+
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (1 != 1) and ("Mene" == "Fofo") 
+
+# Make me true!
+bool_two = 1 == 1
+
+# Make me false!
+bool_three = 1 != 1
+
+# Make me true!
+bool_four = 1 == 1
+
+# Make me true!
+bool_five = 1 == 1
