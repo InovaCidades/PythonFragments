29886814d64916e3b70b97be5eda7d9681ec3a5e
Gabriel Garcia <gabrielgfa@users.noreply.github.com>
2014-05-01 10:11:33 -0300

    Create Exercicio48.py

---
+++ src/CodeAcademy/05 - Conditionals & Control Flow/Exercicio48.py
@@ -0,0 +1,16 @@
+#Write an if statement in the_flying_circus(). It must include:
+
+#if, elif, and else statements;
+#At least one of and, or, or not;
+#A comparator (==, !=, <, <=, >, or >=);
+#Finally, the_flying_circus() must return True when evaluated.
+#Don't forget to include a : after your if statements!
+
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if(3 < 4) and (5 >= 5):
+        return True
+    elif (3 == 4) or (4 == 5):
+        return False
+    else:
+        return True
