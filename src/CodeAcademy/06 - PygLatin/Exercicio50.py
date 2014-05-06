d5ed0897e1eacda36b490ce08bbcb3475c09d4be
Gabriel Garcia Ferraz do Amaral <gabrielgfa1@gmail.com>
2014-05-06 12:57:50 -0300

    Deleted some files.

--- src/CodeAcademy/06 - PygLatin/Exercicio50.py
+++
@@ -1,7 +0,0 @@
-#On line 4, use raw_input("Enter a word:") to ask the user to enter a word. Save the results of raw_input() in a variable called original.
-#Click Save & Submit Code
-#Type a word in the console window and press Enter (or Return).
-
-print 'Welcome to the Pig Latin Translator!'
-
-original = raw_input("Enter a word:")
