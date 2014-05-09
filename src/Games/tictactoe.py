48aacf09aa3227c758d1fdf9b808adf54e1248ee
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-05-09 17:47:15 -0300

    Reorganized the board interface into a vector.

--- src/Games/tictactoe.py
+++ src/Games/tictactoe.py
@@ -7,10 +7,13 @@ def drawBoard(board):
 
     # "board" is a list of 10 strings representing the board (ignore index 0)
     print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
     print('-----------')
     print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('   |   |')
     print('-----------')
     print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
