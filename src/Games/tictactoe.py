67365b490838511b17a1c4c91696e9ae8fb27e1c
Gabriel Garcia <gabrielgfa1@gmail.com>
2014-05-09 17:46:45 -0300

    Imported lib and added some interface prints.

---
+++ src/Games/tictactoe.py
@@ -0,0 +1,16 @@
+# Tic Tac Toe
+
+import random
+
+def drawBoard(board):
+    # This function prints out the board that it was passed.
+
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print('   |   |')
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print('   |   |')
